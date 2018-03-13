b503f0d80d6b694499fe1dca3bd8d1347918451d
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2018-03-12 21:17:13 -0400

    updating tests to work with new stable build after implementing conf file

--- src/tests/clean/test_clean.sh
+++ src/tests/clean/test_clean.sh
@@ -37,7 +37,6 @@ function set_up
 	echo "test2" > $test_dir/test_file2.txt
 	echo "test3" > $test_dir/test_file3.txt
 	echo "test4" > $test_dir/test_file4.txt
-	cp $(cat ../path_to_main.txt)ssh_args.sh ssh_args.sh
 }
 
 ##### TEST CASE #####
@@ -48,7 +47,6 @@ ip=$(curl ipecho.net/plain ; echo)
 clean $path_to_script $ip $host_password $file_to_delete
 verify_clean
 
-rm ssh_args.sh
 ##### determine if we passed all tests #####
 if [ $fail_count == 0 ]; then
 	echo $SUCCESS_STATEMENT
